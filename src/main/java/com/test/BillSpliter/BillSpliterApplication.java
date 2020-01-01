2fe095b3bf7f55b8ee90dca9ea26a47d360f4128
Diwakar Sah <diw_shah@hotmail.com>
2020-01-01 20:06:39 +1100

    Basic structure of bill  splitter in spring boot framework with added configuration

---
+++ src/main/java/com/test/BillSpliter/BillSpliterApplication.java
@@ -0,0 +1,13 @@
+package com.test.BillSpliter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSpliterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSpliterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/BillSpliter/BillSpliterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.BillSpliter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BillSpliterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
